6d21e6f8a89da6cc7ed640b4d039f522ee2b3fcf
Anas <anas.rsmouki.1@gmail.com>
2024-11-05 02:55:25 +0100

    synthèse client.py et gestion exceptions

--- chat_manager.py
+++
@@ -1,8 +0,0 @@
-# chat_manager.py
-import socket
-
-def start_chat_session(user):
-    print(f"Bienvenue dans le chat, {user} !")
-    # Créer un socket ou se connecter à une session de chat existante.
-    # Exemples de commandes : "Envoyer message", "Quitter session", etc.
-    # Utilisation de sockets pour connecter plusieurs utilisateurs.
